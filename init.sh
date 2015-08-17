9a0b775cc77082bdb1bd698d812c939b16fd5edf
Gea-Suan Lin <gslin@gslin.org>
2015-08-17 10:03:34 +0800

    Write to /etc/sysctl.conf directly.

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cat > /etc/sysctl.d/99-tcp-keepalive.conf <<EOF
+cat >> /etc/sysctl.conf <<EOF
 net.ipv4.tcp_keepalive_intvl = 10
 net.ipv4.tcp_keepalive_probes = 3
 net.ipv4.tcp_keepalive_time = 1
 EOF
 
-sysctl --system
+sysctl -p
